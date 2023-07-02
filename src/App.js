23a6bda42555357d10e1255401ace2d8c097e1f7
skskdmsdl <skskdmsdl@gmail.com>
2023-07-02 15:24:22 +0900

    atomize 설치

--- src/App.js
+++ src/App.js
@@ -2,12 +2,17 @@ import React from "react"
 import "tailwindcss/lib/css/preflight.css"
 import AnimationRevealPage from "helpers/AnimationRevealPage"
 import Hero from "components/hero/FullWidthWithImage"
+import { StyleReset, Icon } from "atomize"
+
 
 function App() {
   return (
     <AnimationRevealPage>
       <Hero />
+      {/* <StyleReset /> */}
+      <Icon name="Add" size="10px" />
     </AnimationRevealPage>
+
   )
 }
 
---
+++ src/views/Atomize.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { StyleReset } from 'atomize';
+import App from '../App';
+
+export default function Main() {
+  return (
+    <>
+      <StyleReset />
+      <App />
+    </>
+  );
+}
