0783b2cc3a0083b5bf604311edec4c2c377e5832
skskdmsdl <skskdmsdl@gmail.com>
2023-07-03 22:58:32 +0900

    treact inner page 가져오기, 첫 화면 login 페이지로 설정

--- src/App.js
+++ src/App.js
@@ -2,15 +2,16 @@ import React from "react"
 import "tailwindcss/lib/css/preflight.css"
 import AnimationRevealPage from "helpers/AnimationRevealPage"
 import Hero from "components/hero/FullWidthWithImage"
+import Login from "pages/Login"
 import { StyleReset, Icon } from "atomize"
 
 
 function App() {
   return (
     <AnimationRevealPage>
-      <Hero />
+      <Login />
       {/* <StyleReset /> */}
-      <Icon name="Add" size="10px" />
+      {/* <Icon name="Add" size="10px" /> */}
     </AnimationRevealPage>
 
   )
