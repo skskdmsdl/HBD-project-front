1403e0f3b4d2361d15f721ae80f05bd7d0c6dcdb
skskdmsdl <skskdmsdl@gmail.com>
2023-07-24 22:33:58 +0900

    router 추가

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "tailwindcss/lib/css/preflight.css"
 import AnimationRevealPage from "helpers/AnimationRevealPage"
 import Login from "views/Login"
 import { StyleReset, Icon } from "atomize"
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 function App() {
   const [data, setData] = useState('');
@@ -15,14 +16,15 @@ function App() {
   }, []);
 
   return (
-    <>
-    <AnimationRevealPage>
-
-      <Login data={data}/>
-      {/* <StyleReset /> */}
-      {/* <Icon name="Add" size="10px" /> */}
-    </AnimationRevealPage>
-    </>
+    <BrowserRouter>
+      <Switch>
+        <AnimationRevealPage>
+          <Login data={data}/>
+          {/* <StyleReset /> */}
+          {/* <Icon name="Add" size="10px" /> */}
+        </AnimationRevealPage>
+      </Switch>
+    </BrowserRouter>
 
   )
 }
