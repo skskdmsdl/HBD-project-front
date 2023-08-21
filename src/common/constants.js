bb28c7605a73d3d1bd0d948c6648c3f87ddd7dd1
skskdmsdl <skskdmsdl@gmail.com>
2023-08-21 22:36:20 +0900

    fixed untracked files

--- src/common/constants.js
+++ src/common/constants.js
@@ -1,7 +1,9 @@
 const BASE_URL = process.env.REACT_APP_API_BASE_URL;
 const GOOGLE_AUTH_URL = BASE_URL + '/oauth2/authorization/google';
+const REDIRECT_URI = 'http://localhost:3000/oauth2/redirect';
 
 export {
     BASE_URL,
     GOOGLE_AUTH_URL,
+    REDIRECT_URI
 }
